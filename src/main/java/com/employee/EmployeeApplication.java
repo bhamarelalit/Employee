5fb37a0ad41f5ad00b3cbadc7d70f6c5e136ae22
Lalit <lalit_bhamare@yahoo.com>
2024-06-09 10:35:12 +0530

    Decription for project

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeApplication {
 
+    // Spring Boot application for Jenkins Docker and Kubernates Build
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApplication.class, args);
     }
